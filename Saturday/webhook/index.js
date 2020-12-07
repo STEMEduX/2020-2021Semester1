caab54727312d3f14b55db2d13bf0bd5be0a4993
Jun Wang <junwang2003@gmail.com>
2020-12-06 20:44:24 -0600

    Update index.js

--- Saturday/webhook/index.js
+++ Saturday/webhook/index.js
@@ -6,46 +6,48 @@ const bodyParser = require("body-parser");
 const restService = express();
 
 restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 restService.use(bodyParser.json());
 restService.post("/color", function (req, res) {
-  var speech =
-    req.body.queryResult &&
-      req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.color
-      ? req.body.queryResult.parameters.color
-      : "Seems like some problem. Speak again.";
+    var speech =
+        req.body.queryResult &&
+            req.body.queryResult.parameters &&
+            req.body.queryResult.parameters.color
+            ? req.body.queryResult.parameters.color
+            : "Seems like some problem. Speak again.";
 
-  var speechResponse = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: "Your lucky number is " + (Math.floor(Math.random() * 11 + 100))
-              //Math.floor(Math.random() * 11); 
+    var answer = "Your lucky number is " + (Math.floor(Math.random() * 11 + 100))
+    var speechResponse = {
+        google: {
+            expectUserResponse: true,
+            richResponse: {
+                items: [
+                    {
+
+                        simpleResponse: {
+                            textToSpeech: answer
+                            //Math.floor(Math.random() * 11); 
+                        }
+                    }
+                ]
             }
-          }
-        ]
-      }
-    }
-  };
+        }
+    };
 
-  return res.json({
-    payload: speechResponse,
-    //data: speechResponse,
-    fulfillmentText: speech,
-    speech: speech,
-    displayText: speech,
-    source: "webhook-color-sample"
-  });
+    return res.json({
+        payload: speechResponse,
+        //data: speechResponse,
+        fulfillmentText: answer,
+        speech: answer,
+        displayText: answer,
+        source: "webhook-color-sample"
+    });
 });
 
 restService.listen(process.env.PORT || 8000, function () {
-  console.log("Server up and listening");
+    console.log("Server up and listening");
 });
